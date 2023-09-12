521d0f205531b7f63d147e48dfde68bab2fdef09
WuHaodong <72510911+lymfwz@users.noreply.github.com>
2023-09-12 17:45:16 +0800

    Update 1312.成为回文串最少插入次数.java

--- dp/1312.成为回文串最少插入次数.java
+++ dp/1312.成为回文串最少插入次数.java
@@ -26,3 +26,51 @@ class Solution {
 /*
 动态规划 - 二维数组
 */
+class Solution {
+    public int minInsertions(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        for(int i=n-2;i>=0;i--){
+            char ch1 = s.charAt(i);
+            for(int j=i+1;j<n;j++){
+                char ch2 = s.charAt(j);
+                if(ch1 == ch2){
+                    dp[i][j] = dp[i+1][j-1]; // 无需修改
+                } else {
+                    dp[i][j] = Math.min(dp[i][j-1], dp[i+1][j]) + 1;
+                }
+            }
+        }
+        return dp[0][n-1];
+    }
+}
+/*
+动态规划 - 一维数组
+*/
+class Solution {
+    public int minInsertions(String s) {
+        int n = s.length();
+        // int[][] dp = new int[n][n];
+        int[] dp = new int[n];
+        int[] pre = new int[n];
+        for(int i=n-2;i>=0;i--){
+            char ch1 = s.charAt(i);
+            pre[i] = dp[i];
+            for(int j=i+1;j<n;j++){
+                char ch2 = s.charAt(j);
+                if(ch1 == ch2){
+                    // dp[i][j] = dp[i+1][j-1]; // 无需修改
+                    dp[j] = pre[j-1];
+                } else {
+                    // dp[i][j] = Math.min(dp[i][j-1], dp[i+1][j]) + 1;
+                    dp[j] = Math.min(dp[j-1], dp[j]) + 1;
+                }
+            }
+            for(int j=i+1;j<n;j++){
+                pre[j] = dp[j];
+            }
+        }
+        // return dp[0][n-1];
+        return dp[n-1];
+    }
+}
