3cf75c034020108773bdf418148285f81e0703d9
WuHaodong <72510911+lymfwz@users.noreply.github.com>
2023-09-14 00:18:56 +0800

    Update 1124.表现良好的最长时间段.java

--- 前缀和/1124.表现良好的最长时间段.java
+++ 前缀和/1124.表现良好的最长时间段.java
@@ -27,5 +27,33 @@ class Solution {
     }
 }
 /*
-感觉滑动窗口的贪心思想更好
+感觉滑动窗口的贪心思想更好(滑动窗口解不了，能解最小的窗口，最大无法扩展)
+使用前缀和：
+1. 前缀和>0 , 返回i+1
+2. 前缀和<0 , 找pre-1 , 看下图的例子即可。
 */
+class Solution {
+    public int longestWPI(int[] hours) {
+        int n = hours.length;
+        int pre = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        int res = 0;
+        for(int i=0;i<n;i++){
+            pre += (hours[i] > 8 ? 1 : -1);
+            if(pre > 0) { // 累计劳累天数 > 0
+                res = Math.max(res, i+1);
+            } else {
+                if(map.containsKey(pre - 1)){
+                    res = Math.max(res, i - map.get(pre - 1));
+                }
+                if(!map.containsKey(pre)){
+                    map.put(pre, i);
+                }
+            }
+        }
+        return res;
+    }
+    // 1 2 3 2 1 0 -1 -2 -3 -2 -1 0 1 2 1 0 1 2 3 4 5 6 7 8 9 10
+    // -1 -2 -3 -2 -1 0
+    //  6  6  6  9  9 9
+}
