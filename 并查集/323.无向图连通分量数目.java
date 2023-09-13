69e170edee4fde05ff2b0ab382d1eb017c017001
WuHaodong <72510911+lymfwz@users.noreply.github.com>
2023-09-13 18:37:19 +0800

    Create 323.无向图连通分量数目.java

---
+++ 并查集/323.无向图连通分量数目.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int countComponents(int n, int[][] edges) {
+        UnionFind uf = new UnionFind(n);
+        int res = n;
+        for(int[] e : edges){
+            int a = e[0];
+            int b = e[1];
+            if(uf.union(a, b)){
+                res--;
+            }
+        }
+        return res;
+    }
+}
+class UnionFind{
+    int[] fa;
+    int[] rank;
+    UnionFind(int m) {
+        fa = new int[m];
+        rank = new int[m];
+        for (int i = 0; i < m; i++) {
+            fa[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    int find(int x) {
+        if (x == fa[x]) {
+            return x;
+        }
+        return fa[x] = find(fa[x]);
+    }
+
+    boolean union(int i, int j) {
+        i = find(i);
+        j = find(j);
+        if (i == j) {
+            return false;
+        }
+        if (rank[i] > rank[j]) {
+            fa[j] = i;
+        } else {
+            if(rank[i] == rank[j]) rank[j]++;
+            fa[i] = j;
+        }
+        return true;
+    }
+    boolean connect(int i, int j){
+        return find(i) == find(j);
+    }
+}
