ab7ed1c608fbb5d3f5473eb21fe06062d1e0717c
WuHaodong <72510911+lymfwz@users.noreply.github.com>
2023-09-17 23:35:17 +0800

    Update 714. 买卖股票的最佳时机含手续费.java

--- dp/714. 买卖股票的最佳时机含手续费.java
+++ dp/714. 买卖股票的最佳时机含手续费.java
@@ -3,8 +3,9 @@ class Solution {
         int n = prices.length;
         int sold = 0, hold = -prices[0];
         for(int i=1;i<n;i++){
+            int temp = sold;
             sold = Math.max(sold, hold + prices[i] - fee);
-            hold = Math.max(hold, sold - prices[i]);
+            hold = Math.max(hold, temp - prices[i]);
         }
         return sold;
     }
