62df09bb3ff01d9d4dc42d79e0d25a1944364c4a
WuHaodong <72510911+lymfwz@users.noreply.github.com>
2023-09-22 20:31:47 +0800

    Update 300.最长递增子序列.java

--- dp/300.最长递增子序列.java
+++ dp/300.最长递增子序列.java
@@ -6,18 +6,16 @@ class Solution {
         int n = nums.length;
         int[] dp = new int[n];
         Arrays.fill(dp, 1);
+        int max = 1;
         for(int i=1;i<n;i++){
             for(int j=0;j<i;j++){
                 if(nums[i] > nums[j]){
                     dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
+            max = Math.max(max, dp[i]);
         }
-        int res = dp[0];
-        for(int d : dp){
-            res = Math.max(res, d);
-        }
-        return res;
+        return max;
     }
 }
 /*
