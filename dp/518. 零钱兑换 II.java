590801bff35c0f3d4b191e077e9924e7349010ee
wuhaodong <72510911+lymfwz@users.noreply.github.com>
2023-09-23 23:00:46 +0800

    Update 518. 零钱兑换 II.java

--- dp/518. 零钱兑换 II.java
+++ dp/518. 零钱兑换 II.java
@@ -1,3 +1,4 @@
+// 二维
 class Solution {
     public int change(int amount, int[] coins) {
         int n = coins.length;
@@ -13,5 +14,28 @@ class Solution {
             }
         }
         return dp[n][amount];
+    }
+}
+
+// 一维
+class Solution {
+    public int change(int amount, int[] coins) {
+        int n = coins.length;
+        // int[][] dp = new int[n+1][amount+1];
+        // dp[0][0] = 1;
+        int[] dp = new int[amount+1];
+        dp[0] = 1;
+        for(int i=1;i<=n;i++) {
+            // dp[i][0] = 1;
+            for(int j=1;j<=amount;j++) {
+                // dp[i][j] = dp[i-1][j];
+                if(j >= coins[i-1]) {
+                    // dp[i][j] += dp[i][j-coins[i-1]];
+                    dp[j] += dp[j-coins[i-1]];
+                }
+            }
+        }
+        // return dp[n][amount];
+        return dp[amount];
     }
 }
